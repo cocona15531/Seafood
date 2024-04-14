939b015c0b09f09dca8a4319b140a1b0ea2287d1
issei.ueda <ron.ron633662@gmail.com>
2024-04-14 22:33:05 +0900

    first commit

---
+++ Seafood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seafood
+//
+//  Created by Issei Ueda on 2024/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
